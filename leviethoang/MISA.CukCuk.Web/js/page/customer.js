f893fb86f834cf6ef85a4af1929b5329ca299479
leviethoang4320 <56534225+leviethoang4320@users.noreply.github.com>
2020-12-12 01:57:56 +0700

    js và jquery buổi 1

---
+++ leviethoang/MISA.CukCuk.Web/js/page/customer.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    alert('welcome to misa');
+    $.ajax({
+        url: 'http://api.manhnv.net/api/customers',
+        method: 'GET',
+        data: null,
+       
+        contentType: 'application/json'
+    }).done(function (response) {
+        console.log(response)
+        for (var i = 0; i < response.length; i++) {
+            var item = response[i];
+            var customerCode = item['CustomerCode']; 
+            var trHTML = `<tr>
+                                <td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+<td>${customerCode}</td>
+                          </tr>`
+            $('#bang tr:last').after(trHTML);
+            
+        }
+    }).fail(function (response) {
+        
+    })
+})
